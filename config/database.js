2f26aa0281967a8c92839718994f6618b401949c
Sagar <sagar.tanivade@gmail.com>
2018-12-02 15:14:43 +0530

    Recommit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://Sagar:Vegito@ss4@ds123624.mlab.com:23624/vidjot-prod'}
+    module.exports = {mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds123664.mlab.com:23664/vidjot-prod'}
 }
 else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
